92eff2dde08fe9fb55589adfd861ffaa1e7365d6
Stephan Hense <miami666@openmail.cc>
2019-10-22 10:22:03 +0200

    fg

---
+++ G_03_Zufall/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+User Eingaben durch Zufallswerte simulieren
+ */
+namespace G_03_Zufall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
