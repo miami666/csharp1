cbe6575c55268b51449528aad54194745aaaa53d
Stephan Hense <miami666@openmail.cc>
2019-10-22 15:33:18 +0200

    22/10

--- G_04_GarbageCollector/Program.cs
+++ G_04_GarbageCollector/Program.cs
@@ -10,6 +10,18 @@ namespace G_04_GarbageCollector
     {
         static void Main(string[] args)
         {
+            long mem1 = GC.GetTotalMemory(false);
+            Console.WriteLine(mem1);
+            int[] values = new int[50000];
+            long mem2 = GC.GetTotalMemory(false);
+            long res = mem2 - mem1;
+            Console.WriteLine(mem2);
+            Console.WriteLine(res);
+            values = null;
+            GC.Collect();
+            long mem3 = GC.GetTotalMemory(false);
+            Console.WriteLine(mem3);
+            Console.ReadKey();
         }
     }
 }
