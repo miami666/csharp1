583998f310acaa26812ef0f04cc83345db16d43b
Stephan Hense <miami666@openmail.cc>
2019-10-22 11:00:06 +0200

    custom

--- G_03_Zufall/Program.cs
+++ G_03_Zufall/Program.cs
@@ -29,6 +29,17 @@ namespace G_03_Zufall
             zufallszahl = zufallsGenerator.Next(10);
             Console.WriteLine("Zufallszahl mit nach oben begrenztem, durch uns bestimmten Wert: " + zufallszahl);
 
+            for (int i = 0; i < 10; i++)
+                Console.WriteLine((i + 1) + ".:" + zufallsGenerator.Next(3));
+
+            //Bestimmung von Zufallszahlen die nach oben und unten begrenzt ist
+
+            do
+            {
+                zufallszahl = zufallsGenerator.Next(1, 6);
+                Console.WriteLine("Ergebnis: " + zufallszahl);
+            } while (zufallszahl != 5);
+
             Console.ReadKey();
         }
     }
