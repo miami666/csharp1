97175eca93aef9de312d5ececb30a6966cc20775
Stephan Hense <miami666@openmail.cc>
2019-10-22 10:53:59 +0200

    custom

--- G_03_Zufall/Program.cs
+++ G_03_Zufall/Program.cs
@@ -13,8 +13,21 @@ namespace G_03_Zufall
         static void Main(string[] args)
         {
             // Einfuehren Zufallsgenerators
+
             Random zufallsGenerator = new Random();
             Console.WriteLine("eine erste Zufallszahl zwischen 0 und 4 Mrd: " + zufallsGenerator.Next());
+
+            // im obigen Beispiel wurde der ausgeloste Wert lediglich auf der Konsole ausgegeben
+
+            // Beispiel mit Speichern
+
+            Int64 zufallszahl = zufallsGenerator.Next();
+            Console.WriteLine("zweite Zufallszahl zwischen 0 und 4 Mrd.: " + zufallszahl);
+
+            //Bestimmung Zufallszahl mit nach oben begrenztem, durch uns bestimmten Wert
+
+            zufallszahl = zufallsGenerator.Next(10);
+            Console.WriteLine("Zufallszahl mit nach oben begrenztem, durch uns bestimmten Wert: " + zufallszahl);
 
             Console.ReadKey();
         }
