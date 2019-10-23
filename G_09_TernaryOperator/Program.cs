16f4f0709b6fb9fd81e2041a7fbde7910c5abb8d
Stephan Hense <miami666@openmail.cc>
2019-10-23 09:16:09 +0200

    23/10

--- G_09_TernaryOperator/Program.cs
+++ G_09_TernaryOperator/Program.cs
@@ -10,6 +10,39 @@ namespace G_09_TernaryOperator
     {
         static void Main(string[] args)
         {
+            char c = '+';
+            int a, b;
+
+            if (c == '+')
+                a = 1 + 1;
+            else
+                a = 1 - 1;
+            //condition ? consequent : alternative
+            b = c == '+' ? 1 + 1 : 1 - 1;
+            Console.WriteLine(a + " " + b);
+            int zufall = new Random().Next(-5, 5);
+            string ergebnis1, ergebnis2;
+
+            if (zufall >= 0)
+                ergebnis1 = "not minus";
+            else
+            {
+                if (zufall < 0)
+                    ergebnis1 = "negativ" + deineMudda();
+                else ergebnis1 = deineMudda();
+            }
+            Console.WriteLine(zufall);
+            Console.WriteLine(ergebnis1);
+
+            ergebnis2 = (zufall > 0) ? "größer Null" : (zufall < 0) ? "kleiner Null" + deineMudda() : deineMudda();
+            Console.WriteLine(ergebnis2);
+            Console.ReadKey();
         }
+        static string deineMudda()
+        {
+            return "deineMudda";
+        }
+
+            
     }
 }
