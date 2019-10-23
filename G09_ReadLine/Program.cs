e25aebc1326e3411fa3f07d55d59f40d3fd43652
Stephan Hense <miami666@openmail.cc>
2019-10-23 10:06:33 +0200

    23/10

---
+++ G09_ReadLine/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G09_ReadLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            // zum Einlesen werden wir die Funktion ReadLine() verwenden, die ebenfalls in der Klasse Console vorkommt
+
+            Console.Write("Geben Sie bitte einen Text ein (wird nicht abgespeichert):");
+            Console.ReadLine();
+            // Problem hier: Der bloße Aufruf der Funktion ReadLine() ermöglicht zwar eine Eingabe 
+            // (die durch Return beendet wird) ...
+            // aber: Das System fängt diese Eingabe nicht auf, es erscheint lediglich auf der Konsole
+
+            // LÖSUNG des Problems: Wir speichern den Rückgabewert der Funktion ReadLine() in einem String ab 
+            // (Der Rückgabewert ist nämlich gerade die User-Eingabe)
+
+
+            Console.ReadKey();
+        }
+    }
+}
