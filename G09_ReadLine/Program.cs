16b6941e9be4892ef0e49fd2e98dfd23a1bc9739
Stephan Hense <miami666@openmail.cc>
2019-10-23 11:00:24 +0200

    23/10

--- G09_ReadLine/Program.cs
+++ G09_ReadLine/Program.cs
@@ -21,6 +21,59 @@ namespace G09_ReadLine
 
             // LÖSUNG des Problems: Wir speichern den Rückgabewert der Funktion ReadLine() in einem String ab 
             // (Der Rückgabewert ist nämlich gerade die User-Eingabe)
+            Console.Write("Geben Sie bitte einen Text ein (wird abgespeichert):");
+            text = Console.ReadLine();
+            Console.WriteLine("Control: " + text);
+
+            //Abfragen von ganzen Zahlen
+            Console.WriteLine("Bitte geben sie eine ganze Zahl ein:");
+            text = Console.ReadLine();
+            //klasse convert methode ToInt32
+            int i;
+            i = Convert.ToInt32(text);
+            
+            Console.WriteLine("control: " + i);
+            Console.WriteLine("Bitte geben sie eine weitere  ganze Zahl ein:");
+            i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("control: " + i);
+
+            //Bemerkung
+            //bei  der Klasse convert methode ToInt kommt es bei falschen zeichen zu Format Exception
+            // bei zu großen Zahlen zu overflow exception
+
+            //Abfrage von Kommazahlen
+            double d;
+            Console.WriteLine("Bitte geben sie eine Kommazahl ein");
+            d = Convert.ToDouble(Console.ReadLine()
+                );
+            Console.WriteLine("control double:"+d);
+            d.ToString("F1");
+            Console.WriteLine("to string: "+d);
+
+            //Try Parse
+            Console.WriteLine("Zahl eingeben die mit Try Parse ausgegeben wird");
+            var foo = Console.ReadLine();
+            if (int.TryParse(foo, out int number1))
+            {
+                Console.WriteLine($"{number1} ist eine Zahl");
+            }
+            else
+            {
+                Console.WriteLine($"{foo} ist keine Zahl");
+            }
+
+
+            Console.WriteLine("KommaZahl eingeben die mit Try Parse ausgegeben wird");
+            var doo = Console.ReadLine();
+            if (double.TryParse(doo, out double number2))
+            {
+                Console.WriteLine($"{number2} ist eine Kommazahl");
+            }
+            else
+            {
+                Console.WriteLine($"{foo} is keine Kommazahl");
+            }
+
 
 
             Console.ReadKey();
