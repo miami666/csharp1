6aeaa218728b90ab3cd2ca7c2595cf7708e44dee
miami666 <s.hense@tuta.io>
2019-10-23 02:30:35 +0200

    23/10

--- G_04_GarbageCollector/Program.cs
+++ G_04_GarbageCollector/Program.cs
@@ -10,6 +10,16 @@ namespace G_04_GarbageCollector
     {
         static void Main(string[] args)
         {
+            long mem1=GC.GetTotalMemory(false);
+            Console.WriteLine("memory: "+ mem1);
+            int[] array = new int[50000];
+            long mem2=GC.GetTotalMemory(false);
+            Console.WriteLine("memory nach array initialisierung: "+mem2);
+            array = null;
+            GC.Collect();
+            long mem3 = GC.GetTotalMemory(false);
+            Console.WriteLine("memory GC Collect: " + mem3);
+            Console.ReadKey();
         }
     }
 }
