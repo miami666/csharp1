c2ddd7dcb15cffc7153292e883b4f486d5d544b7
Stephan Hense <miami666@openmail.cc>
2019-10-24 08:26:47 +0200

    jm

---
+++ G_12_Parse_3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_12_Parse_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
