bb94d4fb8547b96da17c84fc2a0d7ec13bafc627
miami666 <s.hense@tuta.io>
2019-11-04 06:08:09 +0100

    03/11

--- cipher/Program.cs
+++ cipher/Program.cs
@@ -7,53 +7,27 @@ using System.Threading.Tasks;
 namespace cipher
 {
     class VCipher
-
     {
-
         public string encrypt(string txt, string pw, int d)
-
         {
-
             int pwi = 0, tmp;
-
             string ns = "";
-
             txt = txt.ToUpper();
-
             pw = pw.ToUpper();
-
             foreach (char t in txt)
-
             {
-
                 if (t < 65) continue;
-
                 tmp = t - 65 + d * (pw[pwi] - 65);
-
                 if (tmp < 0) tmp += 26;
-
                 ns += Convert.ToChar(65 + (tmp % 26));
-
                 if (++pwi == pw.Length) pwi = 0;
-
             }
-
-
-
             return ns;
-
         }
-
     };
-
-
-
     class Program
-
     {
-
         static void Main(string[] args)
-
         {
 
             VCipher v = new VCipher();
