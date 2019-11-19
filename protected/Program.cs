8ea2b485d3ee910ec824e69203c8695d6be249cd
Stephan Hense <miami666@openmail.cc>
2019-11-19 15:21:35 +0100

    19/11

---
+++ protected/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+Zugriffsmodus "protected" (bisher kannten wir nur die Zugriffsmodi "public" und "private")
+        
+Motivation:
+    Wir hatten bereits zu Beginn festgestellt (ohne dies aber weiter zu problematisieren), 
+    dass auf "private" Felder in der Subklasse nicht zugegriffen werden kann.
+    Dies war aus einer Reihe von Gründen oft kein Problem:
+    1.) Falls es zu den entsprechenden privaten Feldern Properties gibt (die ja public sind), 
+        so kann über diese auf die Felder zugegriffen werden.
+    2.) Eine private Variable besitzt zwar keine Property (ist also außerhalb der Klasse tatsächlich nicht verwendbar) wird aber als Wert
+        innerhalb einer öffentlichen Methode der selben Klasse verwendet => die öffentliche Methode wird mit vererbt, kann dann (da sie ja
+        Mitglied der selben Klasse ist, wie die private Variable) auf diese zugreifen und mit ihr arbeiten, so dass wir erneut kein Problem
+        damit haben, dass dieses Feld privat ist.
+    ABER:
+    natürlich könnte es sein, dass eine private Variable innerhalb der Basisklasse für eine Methode bereitsteht und innerhalb der Subklasse
+    für eine andere (nicht geerbte) aber öffentliche Methode verwendet werden soll => die ebende Klasse würde gerne auf diese private Variable zugreifen
+    können, OHNE den Zugriffsmodus von außen zu ändern. (Methoden einer Klasse können ja auf alle Felder der selben Klasse zugreifen, auch wenn diese
+    privat sind, und genau dies sollte "doch eigentlich" auch für die geerbten Felder gelten)
+
+    Zudem (und das soll Gegenstand des folgenden Beispielcodes sein) können wir auch den Wunsch verspüren, dass eine Basisklasse KEINE Instanzen
+    erzeugen kann, aber die Subklasse den entsprechenden Konstruktor (indirekt über base) aufrufen kann.
+
+Lösung des Problems:
+    Wir führen mit dem Zugriffsmodus "protected" einen Modus ein, der a) von außen wie private wirkt und b) aus Sicht der Subklasse wie public 
+        
+*/
+namespace @protected
+{
+    class MyClass_protected
+    {
+        protected MyClass_protected(string s)
+        {
+            Console.WriteLine(s);
+        }
+    }
+    class B3:MyClass_protected
+    {
+        public B3(string s):base(s)
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B3 b = new B3("Yoghtze");
+            Console.ReadKey();  
+        }
+    }
+}
---
+++ protected_aufgabe_1/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+    Führen Sie bitte die folgenden Klassen ein:
+        Entität
+        Gegenstand
+        Lebewesen
+        Tier
+        Mensch
+    Für diese gilt:
+        Gegenstand und Lebewesen erben von Entität / Tier und Mensch erben von Lebewesen
+        Nur von Gegenstand, Tier und Mensch sollen Objekte instanziierbar sein
+    
+    Aufgaben
+        a) Erstellen Sie ein entsprechendes UML-Diagramm (# für protected)
+        b) Für alle Klassen soll eine Liste eingeführt werden
+        c) Alle Objekte sollen in alle passenden Listen eingetragen werden (Menschen also z.B. in der Menschen-, Lebewesen- und Entitäten-Liste)
+        d) führen Sie bitte alle Objekte ein (Werte: siehe Screenshot)
+        e) lassen Sie bitte alle Klassen ausgeben (wie im Screenshot)
+*/
+namespace protected_aufgabe_1
+{
+    class Entitaet
+    {
+        public static List<Entitaet> eListe = new List<Entitaet>();
+        string name;
+        int kg;
+        public string Name
+        {
+            get => name;
+            set
+            {
+            }
+        }
+        public int Kg
+        {
+            get => kg;
+            set
+            {
+
+            }
+        }
+        protected Entitaet()
+        {
+            eListe.Add(this);
+        }
+        protected Entitaet(string n,int kg)
+        {
+            eListe.Add(this);
+            name = n;
+            this.kg = kg;
+        }
+
+    }
+    class Gegenstand:Entitaet
+    {
+        public static List<Gegenstand> gListe = new List<Gegenstand>();
+        string besitzer;
+        public Gegenstand(string b,string n,int kg):base(n,kg)
+        {
+            gListe.Add(this);
+            besitzer = b;
+
+        }
+
+    }
+    class Lebewesen:Entitaet
+    {
+        public static List<Lebewesen> lListe = new List<Lebewesen>();
+        protected string geb;
+        protected Lebewesen(string g)
+        {
+           
+            geb = g;
+            lListe.Add(this);
+
+        }
+
+    }
+    class Tier:Lebewesen
+    {
+        public static List<Tier> tListe = new List<Tier>();
+        string anzahlBeine;
+        public Tier(string beine, string g) : base(g)
+        {
+           
+            anzahlBeine = beine;
+            tListe.Add(this);
+
+        }
+
+    }
+    class Mensch : Lebewesen
+    {
+        public static List<Mensch> mListe = new List<Mensch>();
+        string beruf;
+        public Mensch(string beruf,string g):base(g)
+           
+        {
+            this.beruf = beruf;
+            mListe.Add(this);
+
+        }
+        public static void ZeigemListe()
+        {
+            foreach(var m in mListe)
+            {
+                Console.WriteLine(m.beruf+" "+m.geb);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mensch hans = new Mensch("Zimmermann", "01.01.2000");
+            Mensch.ZeigemListe();
+            Console.ReadKey();
+        }
+    }
+}
