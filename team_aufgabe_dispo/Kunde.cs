f2c5ac356483c92968d742e6c6a220c5daca8ffe
Stephan Hense <miami666@openmail.cc>
2019-11-27 15:17:32 +0100

    27/11

---
+++ team_aufgabe_dispo/Kunde.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * in eigener (extra) Datei einen eigenen namespace für
+{
+- eine abstrakte Klasse "Kunde"
+	* mit abstrakter Methode Login (Ausgabe reicht)
+		(Optional: mit PIN)
+	* mit protected ctor
+- eine public Klasse PrivatKunde, die von Kunde erbt
+- eine public Klasse Geschäftskunde, die von Kunde erbt
+}
+ * */
+namespace my_namespace
+{
+   public abstract class Kunde
+    {
+        public abstract void Login(string pin);
+
+        protected Kunde()
+        {
+
+        }
+    }
+    public class Privatkunde : Kunde
+    {
+        public override void Login(string pin)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+    public class Geschaeftskunde:Kunde
+    {
+        public override void Login(string pin)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
---
+++ team_aufgabe_dispo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using my_namespace;
+/*
+ * Schreiben Sie ein C# Programm welches folgende Aufgaben erfüllt:
+Konto verwaltet werden (für Privatkunden und Geschäftskunden) 
+Erstellung im ctor
+es soll geben
+in eigener (extra) Datei einen eigenen namespace für
+{
+- eine abstrakte Klasse "Kunde"
+	* mit abstrakter Methode Login (Ausgabe reicht)
+		(Optional: mit PIN)
+	* mit protected ctor
+- eine public Klasse PrivatKunde, die von Kunde erbt
+- eine public Klasse Geschäftskunde, die von Kunde erbt
+}
+
+in eigener (extra) Datei einen namespace KontoApp (wie program.cs) für
+{
+eine public Klasse Konto mit Kontonummer, BLZ, Kontoinhaber, Kontostand
+jeweils mit Properties {get; private set;) und ctor
+- Methoden Einzahlen und Abheben (keine negativen Werte)
+}
+
+Dies soll in main durch geeignete Wege getestet werden.
+Optional: mit (Text-)Menü
+Optional2: mit Dispo
+ */
+namespace team_aufgabe_dispo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
