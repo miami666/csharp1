fb16d2af00b19ee4721a627cb9570d96b683cd1b
Stephan Hense <miami666@openmail.cc>
2019-12-19 14:22:37 +0100

    19/12

--- Vokabeltrainer/Vokabeltrainer/MainWindow.xaml.cs
+++ Vokabeltrainer/Vokabeltrainer/MainWindow.xaml.cs
@@ -11,52 +11,161 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using MySql.Data.MySqlClient;
 namespace Vokabeltrainer
 {
+    class DBConnect
+    {
+        static string myConnectionstring = "server=localhost;uid=root;password=;database=vokabeltrainer;";
+        MySqlConnection con = new MySqlConnection(myConnectionstring);
+        public MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        //Constructor
+        public DBConnect()
+        {
+            Initialize();
+        }
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "mitarbeiter";
+            uid = "root";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=";
+            connection = new MySqlConnection(connectionString);
+        }
+        public MySqlConnection verbind()
+        {
+            try
+            {
+                con.Open();
+                MessageBox.Show("Verbindung hergestellt");
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                con = null;
+            }
+            return con;
+        }
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Kann keine Verbindung zum Datenbank Server erstellen");
+                        break;
+                    case 1045:
+                        MessageBox.Show("Benutzername und oder passwort ungültig");
+                        break;
+                }
+                return false;
+            }
+        }
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+        public void ReadData(MySqlConnection con)
+        {
+            var items = new List<Vokabeln>();
+            using (con)
+            {
+                var command = new MySqlCommand("", con);
+                using (var dr = command.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        var item = new Vokabeln()
+                        {
+                            Id = Convert.ToInt32(dr[0]),
+                            Deutsch = Convert.ToString(dr[1]),
+                            Englisch = Convert.ToString(dr[2])
+                        };
+                        items.Add(item);
+                    };
+                };
+            };
+        }
+    }
     public partial class MainWindow : Window
     {
-        Vokabel[] vokabular = new Vokabel[3];
-        Vokabel aktuelleVokabel;
+        Vokabeln aktuelleVokabel;
         Random random = new Random();
-
+        List<Vokabeln> items = new List<Vokabeln>();
         public MainWindow()
         {
             InitializeComponent();
-
-            vokabular[0] = new Vokabel("hallo", "hello");
-            vokabular[1] = new Vokabel("gehen", "to go");
-            vokabular[2] = new Vokabel("gut", "good");
-
-            aktuelleVokabel = vokabular[random.Next(vokabular.Length)];
+            DBConnect db = new DBConnect();
+            MySqlConnection dbcon = db.verbind();
+            // db.ReadData(dbcon);
+            using (dbcon)
+            {
+                var command = new MySqlCommand("select * from vokabeln", dbcon);
+                using (var dr = command.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        var item = new Vokabeln()
+                        {
+                            Id = Convert.ToInt32(dr[0]),
+                            Deutsch = Convert.ToString(dr[1]),
+                            Englisch = Convert.ToString(dr[2])
+                        };
+                        items.Add(item);
+                    };
+                };
+            };
+            lvVokabeln.ItemsSource = items;
+            aktuelleVokabel = items[random.Next(items.Count)];
             labelAbfrage.Content = aktuelleVokabel.HoleDeutschesWort();
         }
-
         private void buttonTest_Click(object sender, RoutedEventArgs e)
         {
             if (aktuelleVokabel.Prüfe(textBoxEingabe.Text))
             {
                 int iMax = -1;
                 double fq = 0.0;
-                for (int i = 0; i < vokabular.Length; i++)
-			    {
-			        if(vokabular[i].WarEineMinuteNichtDran())
+                for (int i = 0; i < items.Count; i++)
+                {
+                    if (items[i].WarEineMinuteNichtDran())
                     {
-                        double fqi = vokabular[i].BerechneFehlerquotient();
-                        if(fqi > fq)
+                        double fqi = items[i].BerechneFehlerquotient();
+                        if (fqi > fq)
                         {
                             fq = fqi;
                             iMax = i;
                         }
                     }
-			    }
+                }
                 if (iMax == -1)
                 {
-                    iMax = random.Next(vokabular.Length);
+                    iMax = random.Next(items.Count);
                 }
-                aktuelleVokabel = vokabular[iMax];
+                aktuelleVokabel = items[iMax];
                 labelAbfrage.Content = aktuelleVokabel.HoleDeutschesWort();
-                textBoxEingabe.Clear(); // nach Videoaufnahme ergänzt
+                textBoxEingabe.Clear();
+                MessageBox.Show("Richtig!");
             }
             else
             {
@@ -64,24 +173,32 @@ namespace Vokabeltrainer
             }
         }
     }
-
-    class Vokabel
+    class Vokabeln
     {
+        public int Id { get; set; }
+        public string Deutsch { get; set; }
+        public string Englisch { get; set; }
+        public override string ToString()
+        {
+            return this.Id + " " + this.Deutsch + " " + this.Englisch;
+        }
         string deutschesWort;
         public string HoleDeutschesWort()
         {
-            return deutschesWort;
+            return Deutsch;
         }
         string englischeÜbersetzung;
+        public string HoleEnglischesWort()
+        {
+            return Englisch;
+        }
         DateTime letzteAbfrage;
         int zahlKorrekterAbfragen;
         int zahlFehlgeschlagenerAbfragen;
-
         public bool WarEineMinuteNichtDran()
         {
             return letzteAbfrage < DateTime.Now - TimeSpan.FromMinutes(1);
         }
-
         public double BerechneFehlerquotient()
         {
             if (zahlKorrekterAbfragen + zahlFehlgeschlagenerAbfragen == 0)
@@ -90,18 +207,19 @@ namespace Vokabeltrainer
             }
             return (double)zahlFehlgeschlagenerAbfragen / (zahlKorrekterAbfragen + zahlFehlgeschlagenerAbfragen);
         }
-
-        public Vokabel(string deutschesWort, string englischeÜbersetzung)
+        public Vokabeln()
         {
-            this.deutschesWort = deutschesWort;
-            this.englischeÜbersetzung = englischeÜbersetzung;
+        }
+        public Vokabeln(string deutschesWort, string englischeÜbersetzung)
+        {
+            this.Deutsch = deutschesWort;
+            this.Englisch = englischeÜbersetzung;
             letzteAbfrage = DateTime.Now;
         }
-
         public bool Prüfe(string p)
         {
             letzteAbfrage = DateTime.Now;
-            if (p == englischeÜbersetzung)
+            if (p == HoleEnglischesWort())
             {
                 zahlKorrekterAbfragen++;
                 return true;
