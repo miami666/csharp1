4e4dbe63ec91694068619fbe2625b42f13003ede
miami666 <s.hense@tuta.io>
2020-01-10 06:39:34 +0100

    1001

--- WpfApp_kcal/MainWindow.xaml.cs
+++ WpfApp_kcal/MainWindow.xaml.cs
@@ -25,11 +25,40 @@ public partial class MainWindow : Window
         public MainWindow()
         {
             InitializeComponent();
-           
-        }
-    
+            //List<Dictionary<string, string>> y = new List<Dictionary<string, string>>();
 
-        
+
+        }
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddKeyValue(txtKey.Text, txtStringValue.Text);
+        }
+        private void AddKeyValue(object key, object value)
+        {
+            // load the resource dictionary
+            var rd = new System.Windows.ResourceDictionary();
+            rd.Source = new System.Uri("pack://application:,,,/Dic.xaml", System.UriKind.RelativeOrAbsolute);
+
+            // add the new key with value
+            rd.Add(key, value);
+            if (rd.Contains(key))
+            {
+                rd[key] = value;
+            }
+            else
+            {
+                rd.Add(key, value);
+            }
+
+            // now you can save the changed resource dictionary
+            var settings = new System.Xml.XmlWriterSettings();
+            settings.Indent = true;
+            var writer = System.Xml.XmlWriter.Create(@"Dic.xaml", settings);
+            System.Windows.Markup.XamlWriter.Save(rd, writer);
+        }
+
+
+
     }
         
     }
---
+++ Wpf_kcal_calc/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace Wpf_kcal_calc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+          
+        }
+        double kcal;
+        string inp;
+        double sum;
+        bool abDafuer=false;
+        Dictionary<string, double> kcalDict = new Dictionary<string, double>();
+
+
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if(kcalValue.Text != "" && speiseKey.Text != "")
+            {
+                inp = speiseKey.Text;
+                Double.TryParse(kcalValue.Text, out kcal);
+                kcalDict.Add(inp, kcal);
+                WriteDictToFile(kcalDict, "Y:\\kcalDict.txt");
+
+            }
+           
+           
+        }
+        private void btnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            abDafuer = true;
+           
+            double sum = kcalDict.Sum(x => x.Value);
+            MessageBox.Show("Da pack ich mir an de Kopp. Du has janz fiese " + sum + " Kalorien jefräße.\nDu mus " + 30 * sum / 50 + " Minuten die Beene vertredde john oder + " + 60 * sum / 400 + " Minuten schwemme.");
+           
+
+        }
+        private void btnNeueSpeise_Click(object sender, RoutedEventArgs e)
+        {
+            speiseKey.Clear();
+            kcalValue.Clear();
+
+        }
+        private void WriteDictToFile(Dictionary<string, double> Dict, string path)
+        {
+            using (StreamWriter fileWriter = new StreamWriter(path))
+            {
+           
+                foreach (KeyValuePair<string, double> kvPair in Dict)
+                {
+                    fileWriter.WriteLine("{0}: {1}", kvPair.Key, kvPair.Value);
+                }
+            }
+        
+        }
+    }
+}
+
